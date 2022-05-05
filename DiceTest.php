bd9eba090355681956b91892fbf94c320f2fe8e1
hidetsugu-o <74892038+hidetsugu-o@users.noreply.github.com>
2022-05-05 09:56:17 +0900

    テストとは

--- DiceTest.php
+++ DiceTest.php
@@ -12,8 +12,27 @@ class DiceTest extends TestCase
         $this->dice = new Dice();
     }
 
-    public function testInstanceOf() 
+    public function testInstanceOf()
     {
         $this->assertInstanceOf(Dice::class, $this->dice);
+    }
+
+    public function testSided()
+    {
+        $this->assertCount(6, $this->dice->getSided());
+        for ($i = 1; $i <= 6; $i++) {
+            $this->assertContains($i, $this->dice->getSided());
+        }
+
+        return $this->dice;
+    }
+
+    public function testMillionRoll()
+    {   
+        for ($i = 1; $i <= 1000000; $i++)
+        {
+            $this->dice->roll();
+            $this->assertTrue(1 <= $this->dice->getNumber() && 1 <= $this->dice->getNumber());
+        }
     }
 }
--- dice.php
+++ dice.php
@@ -1,12 +1,28 @@
 <?php
 
+const SIDED = [1, 2, 3, 4, 5, 6];
+
 class Dice {
     protected array $sided;
     protected int $number;
 
     public function __construct()
     {
+        $this->sided = SIDED;
+    }
 
+    public function getSided() :array
+    {
+        return $this->sided;
+    }
+
+    public function roll() :void
+    {
+        $this->number = $this->sided[array_rand($this->sided)];
+    }
+
+    public function getNumber() :int
+    {
+        return $this->number;
     }
 }
-
